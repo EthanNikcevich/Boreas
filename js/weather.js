0226d099fee3b2339e00ac5b364490191cb28124
Arik Talmon <Ariktalmon@students.berkeley.net>
2017-05-17 11:26:15 -0700

    hi

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,10 @@
-/**
- * Created by h205p3 on 5/17/17.
- */
-wasssssaaaap
+$.ajax({
+    url: "https://api.darksky.net/forecast/ad0a25918602e1d5ac8a98c6c9764da1/37.8267,-122.4233",
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'json',
+    success: function(result) { console.log(result) },
+    error: function() { alert('Failed!'); }
+});
+
+
