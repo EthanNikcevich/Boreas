c5537b2e142c94f4358463f790972f51d8002b6e
EthanNikcevich <ethannikcevich@students.berkeley.net>
2017-05-19 11:17:28 -0700

    hi

--- js/sam.js
+++ js/sam.js
@@ -1,3 +1,18 @@
 /**
  * Created by h205p3 on 5/17/17.
  */
+$(document).ready(function(){
+
+});
+//80<F = HOT
+//60<F<=80 = WARM
+//35<F<=60 = COLD
+//F<=35 = FRIGID
+
+//clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
+
+//snow: Stay inside and have some hot coco
+//sleet: Stay inside and have some hot coco
+//clear-day & HOT: If you're going out, remember to put on sunscreen. It's hot today. Remember to hydrate!
+//clear-day & WARM: Have fun! Go outside and enjoy yourself. It's a lovely day!
+//clear-day & COLD: It's a beautiful day! Remember to wear a sweater if you're going outside. It's cold!
