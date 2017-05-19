8887cbfb7d1dd1faf7ee25d0c742dca6552f8082
EthanNikcevich <ethannikcevich@students.berkeley.net>
2017-05-19 11:33:08 -0700

    hi again x2

--- js/ethan.js
+++ js/ethan.js
@@ -11,8 +11,26 @@ $(document).ready(function(){
 
 //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
 
+
+//rain: Bring an umbrella! You don't want to get wet.
 //snow: Stay inside and have some hot coco
 //sleet: Stay inside and have some hot coco
+//wind & HOT: Fly a kite and dress light. It's hot out there.
+//wind & WARM: It's a good day to go out for a trip to the beach. Have fun.
+//wind & COLD: Bring a windbreaker if you're going out. Kite flying is really fun!
+//wind & FRIGID: Stay inside. Seriously. It's cold af out there.
+//fog & HOT: If you're going out, remember to put on sunscreen. It's hot today. Remember to hydrate!
+//fog & WARM: It's the perfect day for a run in the outdoors. Have fun out there!
+//fog & COLD: Remember to wear a sweater if you're going outside. It's cold!
+//fog & FRIGID: Stay inside and have some hot coco.
+//cloudy & HOT: If you're going out, wear shorts and a t-shirt. It's hot today. Remember to hydrate!
+//cloudy & WARM: It's the perfect day for a run in the outdoors. Have fun out there!
+//cloudy & COLD: Remember to wear a sweater if you're going outside. It's cold!
 //clear-day & HOT: If you're going out, remember to put on sunscreen. It's hot today. Remember to hydrate!
 //clear-day & WARM: Have fun! Go outside and enjoy yourself. It's a lovely day!
 //clear-day & COLD: It's a beautiful day! Remember to wear a sweater if you're going outside. It's cold!
+//clear-day & FRIGID: The sky is clear, and water is freezing over. Wear a coat, or stay inside and have some hot coco.
+//clear-night & HOT: If you're going out, there's no need to bring a jacket. Be careful, it's dark out.
+//clear-night & WARM: If you're going out, there's no need to bring a jacket. Be careful, it's dark out.
+//clear-night & COLD: If you're going out, bring a coat. It's cold out there.
+//clear-night & FRIGID: Stay inside and have some hot coco by the fire.
