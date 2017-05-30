a0abaf4244215a069a1e3aa2afa4f38f9296096d
Morris Fuller <morrisjfuller@gmail.com>
2017-05-30 10:52:07 -0700

    Second page!

--- js/ben.js
+++ js/ben.js
@@ -39,4 +39,4 @@ function page2Background() {
 //Potential GIFs
 //storm gif https://s-media-cache-ak0.pinimg.com/originals/ac/50/19/ac501913ebf72f75898e7d5c32e09c17.gif
 //rain gif https://media.tenor.co/images/7814051a512a18d4d4c90197b97ce741/tenor.gif
-//
+// aelrubaoieurbvpieurb
